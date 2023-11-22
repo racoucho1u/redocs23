4fb5fc9c479e4ec2e2dc24ce19409b6e05f71328
mracouch <maiwenn.racouchot@inria.fr>
2023-11-22 19:20:23 +0100

    Commeeeeeeeeeeeeeeeeeeeeeeeents

--- estimate_time.py
+++ estimate_time.py
@@ -14,10 +14,11 @@ def guessTimeNode(nuuid,nodes_estimated,floating_nodes):
 	#If the first_seen is not usable
 	if (not g.nodes_dict[nuuid].first_seen()):
 
+		#Get the first appearences of the edges connected to the node
 		allFst = [e.estimated_first for e in eto+efrom]
 		if (len(allFst)>0):
 
-			mini = min(allFst)
+			mini = min(allFst)	#Get the earliest of all
 
 			#Track wether the node will be changed
 			if (mini != g.nodes_dict[nuuid].estimated_first):
@@ -65,12 +66,14 @@ def guessTimeEdge(euuid,edges_estimated):
 	#If we need to make a guess because the time of the edge cannot be used
 	if (not etime):
 
+		#Estimated time is by delault the time of the node if it exitst (and is legit ie. not the time of analysis)
+		#otherwise it will be the first (respectively last) time available in the graph (that is not the analysis time)
 		from_time_fst = nfrom.estimated_first
 		to_time_last = nto.estimated_last
 		from_uuid = nfrom.uuid
 		to_uuid = nto.uuid
 
-		#If the node before has a usable seen_first time
+		#If the node before has a usable seen_first time, and it is more precise than the current time, change it
 		if (from_time_fst > g.edges_dict[euuid].estimated_first):
 			g.edges_dict[euuid].estimated_first = from_time_fst
 			changed = True
@@ -92,53 +95,58 @@ def lauchAnalysis(mu,gatherFnodes):
 	nodes_estimated = []
 	floating_nodes = []
 
+	#Go through all edges to learn from their surroundings
 	for euuid in g.edges_dict:
 		edges_estimated = guessTimeEdge(euuid,edges_estimated)
+	#Go through all nodes to learn from their surroundings
 	for nuuid in g.nodes_dict:
 		(nodes_estimated,floating_nodes) = guessTimeNode(nuuid,nodes_estimated,floating_nodes)
 
-	#with open("nodes_estimated.log","a") as ne:
-	#	for n in nodes_estimated:
-	#		ne.write(f"Node: {n[0]} | estimated_first: {n[1]} | estimated_last: {n[2]}")
-	#print(f"Changed nodes: {len(nodes_estimated)}")
-
-	#with open("edges_estimated.log","a") as ee:
-	#	for e in edges_estimated:
-	#		ee.write(f"Edge: {e[0]} | estimated_first: {e[1]} | estimated_last: {e[2]}")
-	#print(f"Changed edges: {len(edges_estimated)}")
-
-	with open("nodes_estimated.csv","a") as ne:
-		csv_w = csv.writer(ne, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		csv_w.writerow([f"node.uuid", f"node.estimated_first", f"node.estimated_last"])
+	with open("nodes_estimated.log","a") as ne:
 		for n in nodes_estimated:
-			csv_w.writerow([f"{n[0]}", f"{n[1]}", f"{n[2]}"])
+			ne.write(f"Node: {n[0]} | estimated_first: {n[1]} | estimated_last: {n[2]}")
+	print(f"Changed nodes: {len(nodes_estimated)}")
 
-	with open("edges_estimated.csv","a") as ee:
-		csv_w = csv.writer(ee, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		csv_w.writerow([f"edge.uuid", f"edge.estimated_first", f"edge.estimated_last"])
+	with open("edges_estimated.log","a") as ee:
 		for e in edges_estimated:
-			csv_w.writerow([f"{e[0]}", f"{e[1]}", f"{e[2]}"])
+			ee.write(f"Edge: {e[0]} | estimated_first: {e[1]} | estimated_last: {e[2]}")
+	print(f"Changed edges: {len(edges_estimated)}")
+
+
+	### Write modified nodes and edges as a csv
+
+	#with open("nodes_estimated.csv","a") as ne:
+	#	csv_w = csv.writer(ne, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	#	csv_w.writerow([f"node.uuid", f"node.estimated_first", f"node.estimated_last"])
+	#	for n in nodes_estimated:
+	#		csv_w.writerow([f"{n[0]}", f"{n[1]}", f"{n[2]}"])
+
+	#with open("edges_estimated.csv","a") as ee:
+	#	csv_w = csv.writer(ee, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	#	csv_w.writerow([f"edge.uuid", f"edge.estimated_first", f"edge.estimated_last"])
+	#	for e in edges_estimated:
+	#		csv_w.writerow([f"{e[0]}", f"{e[1]}", f"{e[2]}"])
 
 	if (gatherFnodes):
 		with open("floating_nodes.log","a") as fn:
 			for n in floating_nodes:
 				fn.write(n+"\n")
 
-	print(len(changed_nodes))
-
-	mu.append(g.mu())
+	mu.append(g.mu())				#Compute new mu value after the graph update
 	print(mu)
-	delta = mu[-2]-mu[-1]	
+	delta = mu[-2]-mu[-1]			#Compute the new delta to see if we have gain knowledges
 	print(f"New delta: {delta}")
 	return(delta)
 
-mu = [g.mu()]
-delta = mu[0]
-gatherFnodes = True
-changed_nodes = []
+#Initialization
+mu = [g.mu()]				#Measure over the graph (total difference between fst and last estimated times)
+delta = mu[0]				#Initialisation of the difference between two mu value
+gatherFnodes = True			#Set to false if you don't want to gather the floating nodes in a file
+
+# As long as we learn things (it will move the delta), threshold can be moved to fit the graph
 while (delta > 0):
 	delta = lauchAnalysis(mu,gatherFnodes)
-	gatherFnodes = False
+	gatherFnodes = False	#Floating nodes need gathering only once
 
 
 g.save()
